d6e3ccd1d45b8785f0b991498b4468292c7628c0
vinso0 <arvinsocao2005@gmail.com>
2025-10-16 16:33:53 +0800

    Updates

---
+++ ClothingOrderAndStockManagement.Application/Dtos/Packages/UpdatePackageDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothingOrderAndStockManagement.Application.Dtos.Packages
+{
+    public class UpdatePackageDto
+    {
+        public int PackagesId { get; set; }
+        public string PackageName { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public List<UpdatePackageItemDto> PackageItems { get; set; } = new();
+    }
+
+    public class UpdatePackageItemDto
+    {
+        public int ItemId { get; set; }
+        public int ItemQuantity { get; set; }
+    }
+}
