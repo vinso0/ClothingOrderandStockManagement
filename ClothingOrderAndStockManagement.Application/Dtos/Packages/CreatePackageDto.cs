bfb94b38d177851a6ed334d919189b35c50c6e26
vinso0 <arvinsocao2005@gmail.com>
2025-11-03 23:58:27 +0800

    Updated Packages Views to handle available quantity

--- ClothingOrderAndStockManagement.Application/Dtos/Packages/CreatePackageDto.cs
+++ ClothingOrderAndStockManagement.Application/Dtos/Packages/CreatePackageDto.cs
@@ -5,6 +5,7 @@
         public string PackageName { get; set; } = string.Empty;
         public string? Description { get; set; }
         public decimal Price { get; set; }
+        public int QuantityAvailable { get; set; } = 0;
         public List<CreatePackageItemDto> PackageItems { get; set; } = new();
     }
 
